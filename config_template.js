c2ab6d15d7bccd30711115ae9b7356b33170b170
jjjing <101921087+jinggg1123@users.noreply.github.com>
2026-09-06 15:59:32 +0800

    Create config_template.js for scheduling parameters
    Add configuration template with parameters for scheduling.

---
+++ config_template.js
@@ -0,0 +1,46 @@
+{
+  "1": {
+    "rtn": "0",
+    "tag": "#milkloveTAG1\n#milkloveTAG2",
+    "year": "2026",
+    "month": "09",
+    "day": "07",
+    "hour": "00",
+    "minute": "00",
+    "continued_time": "24",
+    "extraTag": "#extraTag",
+    "start_time_extraTag": "12",
+    "medias": []
+  },
+  "2": {
+    "rtn": "0",
+    "tag": "#milkloveTag333",
+    "year": "2026",
+    "month": "09",
+    "day": "16",
+    "hour": "00",
+    "minute": "00",
+    "continued_time": "24",
+    "extraTag": "",
+    "start_time_extraTag": "12",
+    "medias": [
+      "https://x.com/TheStandardPOP/status/1922597223621181712/photo/1",
+      "https://x.com/ZZocial/status/1922587346710479051/video/1"
+    ]
+  }
+}
+
+
+//每个参数的解释
+"rtn": "0",     默认为0
+"tag": "#milkloveTag333",   主tag标签内容,换行符是/n
+"year": "2026",     定时开始的年份
+"month": "09",      定时开始的月份
+"day": "16",        定时开始的日期
+"hour": "00",       定时开始的小时
+"minute": "00",     定时开始的分钟
+"continued_time": "24", 持续时间（小时）
+"extraTag": "",    额外标签
+"start_time_extraTag": "12", 额外标签延迟开始时间（小时）
+"medias": [] 媒体链接,定时会带这些媒体
+
