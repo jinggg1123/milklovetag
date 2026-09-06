426250b8ac87a4d6b9ad1e76c1c2c6ba66796e73
jjjing <101921087+jinggg1123@users.noreply.github.com>
2026-09-06 15:31:24 +0800

    Add initial configuration in config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+{
+  "1": {
+    "rtn": "0",
+    "tag": "#milkloveTAG1\n#milkloveTAG2",
+    "year": "2026",
+    "month": "09",
+    "day": "07",
+    "hour": "00",
+    "minute": "00",
+    "continued_time": "24",
+    "extraTag": "#extraTag",
+    "start_time_extraTag": "12",
+    "medias": []
+  }
+}
